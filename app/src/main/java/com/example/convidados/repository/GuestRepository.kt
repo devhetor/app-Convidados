254b1db2d76c8147a71dcbd2742353cb45f1c28f
Heitor <heitorbrandolin@hotmail.com>
2023-07-20 11:53:16 -0300

    Update & Delete

--- app/src/main/java/com/example/convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -39,5 +39,38 @@ class GuestRepository private constructor(context: Context) {
         }
     }
 
+    fun update(guest: GuestModel): Boolean{
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val presence = if (guest.presence) 1 else 0
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+
+            val selection =  DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+            true
+        }catch (e: Exception){
+            false
+        }
+    }
+
+    fun delete(id: Int): Boolean{
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val selection =  DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
+            true
+        }catch (e: Exception){
+            false
+        }
+    }
 
 }
